4dd714669906525518bf9db48532c18a484d9f87
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-03 15:59:04 -0400

    set up schema

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE database employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT NOT NULL auto-increment PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+  Id INT NOT NULL auto-increment PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INTEGER,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+  Id INT NOT NULL auto-increment PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INTEGER,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES role(id)
+);
+
+

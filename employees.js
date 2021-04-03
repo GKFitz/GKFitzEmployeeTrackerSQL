96cec15f5ce74cb04c2d4a69fa18e50e71e255ef
Gillian Fitzgerald <gillianfitzgerald@gillians-mbp.home>
2021-04-03 16:53:57 -0400

    connected my sql

--- employees.js
+++ employees.js
@@ -1,4 +1,27 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
+ 
+// create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: "chocolate1",
+  database: 'employees_db'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("mysql connected")
+    //runSearch();
+});
+
+//create department
+
+async function createDepartment(name) {
+    let query= `INSERT into department (name) VALUES ("${name}")`;
+    connection.query()
+
+}
+
 
